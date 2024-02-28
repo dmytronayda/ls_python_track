790db107d2d37c3d3a8718b805d56dc759b942e0
Dima Naida <dima.nayda.1993@gmail.com>
2024-02-28 12:36:18 +0100

    Added solutions for basic operations and variables.

---
+++ intro_to_programming_with_python_book/basic_operations.py
@@ -0,0 +1,4 @@
+first_name = "Dima"
+last_name = "Naida"
+SPACE = " "
+print(f'My full name is: {first_name + SPACE + last_name}')
---
+++ intro_to_programming_with_python_book/greeter.py
@@ -0,0 +1,7 @@
+name = "Victor"
+
+def greeter(name):
+    for part_of_day in ["Morning", "Afternoon", "Evening"]:
+        print(f"Good {part_of_day}, {name}.")
+
+greeter(name)
