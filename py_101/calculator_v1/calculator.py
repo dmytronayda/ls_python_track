ef4e641489b284505d8a7c1811e293c2bd51cc49
Dima Naida <dima.nayda.1993@gmail.com>
2024-03-24 17:56:16 +0100

    Add German language support for calculator.

--- py_101/calculator_v1/calculator.py
+++ py_101/calculator_v1/calculator.py
@@ -1,5 +1,17 @@
+import json
+
+with open("calculator_messages.json", "r") as file:
+    MESSAGES = json.load(file)
+
+LANGUAGE = "de"
+
+
 def prompt(message):
     print(f"=> {message}")
+
+
+def messages(message, lang="en"):
+    return MESSAGES[lang][message]
 
 
 def invalid_number(num_str):
@@ -11,38 +23,49 @@ def invalid_number(num_str):
     return False
 
 
-prompt("Welcome to Calculator!")
-
-prompt("What's the first number?")
-number1 = input()
-while invalid_number((number1)):
-    prompt("Hmm... that doesn't look like a valid number.")
+def calculate():
+    prompt(messages("first_number", LANGUAGE))
     number1 = input()
+    while invalid_number((number1)):
+        prompt(messages("invalid_number_error", LANGUAGE))
+        number1 = input()
 
-prompt("What's the second number?")
-number2 = input()
-while invalid_number((number2)):
-    prompt("Hmm... that doesn't look like a valid number.")
+    prompt(messages("second_number", LANGUAGE))
     number2 = input()
+    while invalid_number((number2)):
+        prompt(MESSAGES["invalid_number_error"])
+        number2 = input()
 
-print(f"num 1 = {number1} and num2 = {number2}")
+    print(f"num 1 = {number1} and num2 = {number2}")
 
-prompt(
-    "What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide"
-)
-operation = input()
-while operation not in ["1", "2", "3", "4"]:
-    prompt("You must choose 1, 2, 3, or 4")
+    prompt(messages("operations", LANGUAGE))
     operation = input()
+    while operation not in ["1", "2", "3", "4"]:
+        prompt(messages("invalid_operation_error", LANGUAGE))
+        operation = input()
 
-match operation:
-    case "1":
-        result = int(number1) + int(number2)
-    case "2":
-        result = int(number1) - int(number2)
-    case "3":
-        result = int(number1) * int(number2)
-    case "4":
-        result = int(number1) / int(number2)
+    match operation:
+        case "1":
+            result = int(number1) + int(number2)
+        case "2":
+            result = int(number1) - int(number2)
+        case "3":
+            result = int(number1) * int(number2)
+        case "4":
+            result = int(number1) / int(number2)
 
-prompt(f"The result is {result}")
+    prompt(messages("result", LANGUAGE) + str(result))
+
+
+prompt(messages("welcome", LANGUAGE))
+need_calculation = True
+
+while need_calculation:
+    calculate()
+    prompt(messages("calculate_again", LANGUAGE))
+    user_input = input().capitalize()
+    while user_input not in messages("calculate_again_options", LANGUAGE):
+        prompt(messages("invalid_option_error", LANGUAGE))
+        user_input = input().capitalize()
+    if user_input == "N":
+        need_calculation = False
